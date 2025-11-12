e68cef0c3ba9dfb94deb7c2acfc49676b95fdf7a
Levi Gleik <leviacedo1@gmail.com>
2025-11-12 16:41:29 -0300

    feat: add shadcn

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,32 +1,46 @@
 "use client";
-import { cn } from "@heroui/theme";
-import { Navbar } from "@/components/global/navbar/Navbar";
-import { Sidebar } from "@/components/global/sidebar/Sidebar";
-import { useSidebar } from "@/hooks/useSidebar";
-import { useStore } from "@/hooks/useStore";
+import { AppSidebar } from "@/components/global/app-sidebar";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbList,
+	BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-	const sidebar = useStore(useSidebar, (x) => x);
-	if (!sidebar) return null;
-	const { isOpen, toggleOpen } = sidebar;
-
 	return (
-		<div className="flex flex-col h-screen">
-			<Navbar isOpen={isOpen} toggleOpen={toggleOpen} />
-			<main
-				className={cn(
-					"flex flex-1 overflow-hidden",
-					// !settings.disabled && (!getOpenState() ? "lg:ml-[90px]" : "lg:ml-72"),
-				)}
-			>
-				<Sidebar />
-
-				<div className="flex-1 overflow-y-auto">
-					<div className="flex flex-col max-w-[1352px] p-6 gap-6 md:gap-10 mx-auto">
+		<SidebarProvider>
+			<AppSidebar variant="inset" />
+			<SidebarInset>
+				<div className="@container/main min-[56rem]:overflow-y-auto">
+					<header className="sticky top-0 z-50 flex h-(--header-height) shrink-0 items-center gap-2 rounded-t-xl border-b bg-background/90 backdrop-blur-xs transition-[width,height] ease-linear">
+						<div className="flex items-center gap-2 px-4">
+							<SidebarTrigger className="-ml-1" />
+							<Separator
+								orientation="vertical"
+								className="mr-2 data-[orientation=vertical]:h-4"
+							/>
+							<Breadcrumb>
+								<BreadcrumbList>
+									<BreadcrumbItem>
+										<BreadcrumbPage>Transfer</BreadcrumbPage>
+									</BreadcrumbItem>
+								</BreadcrumbList>
+							</Breadcrumb>
+						</div>
+					</header>
+					<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
 						{children}
+						<div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
 					</div>
 				</div>
-			</main>
-		</div>
+			</SidebarInset>
+		</SidebarProvider>
 	);
 }
--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,5 +1,23 @@
-"use client";
+import { AppSidebar } from "@/components/global/app-sidebar";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
 
-export default function Test() {
-	return <div>Teste</div>;
+export default function Page() {
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quasi.
+		</div>
+	);
 }
